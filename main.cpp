3ca204dd47ac3a7bea2c47ebc1ac57d46d372018
boombuster2000 <chanuka.williams@outlook.com>
2024-04-14 13:40:40 +0100

    Draws game title to screen

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include "raylib.h"
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
+#define WINDOW_TITLE "Tile Tapping 2"
  
-int main(int argc, const char **argv) {
-    // Initialization
-    const char *windowTitle = "Tile Tapping 2";
- 
-    InitWindow(SCREEN_HEIGHT, SCREEN_WIDTH, windowTitle);
+int main(int argc, const char **argv) { 
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(60);
  
     // Main loop
     while(!WindowShouldClose()) {
- 
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
 
+        DrawText(WINDOW_TITLE, SCREEN_WIDTH/2, 100, 50, PURPLE);
+
+        EndDrawing();
     }
     // Cleanup
     CloseWindow();
-     
     return 0;
 }
