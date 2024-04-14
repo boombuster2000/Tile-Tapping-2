f221db58803b3ae312bf50dda64b8c99908894db
boombuster2000 <chanuka.williams@outlook.com>
2024-04-14 14:20:32 +0100

    made struct to group text properties and updated GetTextCenterPositionOnScreen  to use text struct

--- main.cpp
+++ main.cpp
@@ -3,17 +3,25 @@
 #define WINDOW_HEIGHT 720
 #define WINDOW_TITLE "Tile Tapping 2"
 
+struct Text
+{
+    const char* text;
+    int fontSize;
+    Color colour;
+    Vector2 position;
+};
+
+
 /**
  * Calculates the centre position on the screen from the given text and font size.
  * 
- * @param text The text that will be displayed.
- * @param fontSize The fontSize being used for the text.
+ * @param text The text struct that will be measured.
  * 
  * @return Vector2 of x and y positions.
 */
-Vector2 GetTextCenterPositionOnScreen(const char* text, const int fontSize)
+Vector2 GetTextCenterPositionOnScreen(const Text text)
 {
-    const int textWidth = MeasureText(text, fontSize);
+    const int textWidth = MeasureText(text.text, text.fontSize);
     const float offset = textWidth/2;
 
     return {((float)GetScreenWidth()/2) - offset, (float)GetScreenHeight()/2};
@@ -29,8 +37,12 @@ int main(int argc, const char **argv)
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        Vector2 TextPosition = GetTextCenterPositionOnScreen(WINDOW_TITLE, 50);
-        DrawText(WINDOW_TITLE, TextPosition.x, 0, 50, PURPLE);
+
+        Text menuTitle = {WINDOW_TITLE, 50, PURPLE};
+        menuTitle.position.x = GetTextCenterPositionOnScreen(menuTitle).x;
+        menuTitle.position.y = 10;
+
+        DrawText(menuTitle.text, menuTitle.position.x, menuTitle.position.y, menuTitle.fontSize, menuTitle.colour);
 
         EndDrawing();
     }
