3a83fbaa0cf1e05f9e124b6b88a255af843c7225
boombuster2000 <chanuka.williams@outlook.com>
2024-04-14 14:08:24 +0100

    Created function to get centre position on screen for text

--- main.cpp
+++ main.cpp
@@ -2,13 +2,22 @@
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 #define WINDOW_TITLE "Tile Tapping 2"
- 
-struct Vector2
-{
-    int x = 0;
-    int y = 0;
-};
 
+/**
+ * Calculates the centre position on the screen from the given text and font size.
+ * 
+ * @param text The text that will be displayed.
+ * @param fontSize The fontSize being used for the text.
+ * 
+ * @return Vector2 of x and y positions.
+*/
+Vector2 GetTextCenterPositionOnScreen(const char* text, const int fontSize)
+{
+    const int textWidth = MeasureText(text, fontSize);
+    const float offset = textWidth/2;
+
+    return {((float)GetScreenWidth()/2) - offset, (float)GetScreenHeight()/2};
+}
 
 int main(int argc, const char **argv) 
 { 
@@ -20,8 +29,8 @@ int main(int argc, const char **argv)
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-
-        DrawText(WINDOW_TITLE, WINDOW_WIDTH/2, 100, 50, PURPLE);
+        Vector2 TextPosition = GetTextCenterPositionOnScreen(WINDOW_TITLE, 50);
+        DrawText(WINDOW_TITLE, TextPosition.x, 0, 50, PURPLE);
 
         EndDrawing();
     }
