f057308257e9acb84ae4b28cb4e8967bead86093
boombuster2000 <chanuka.williams@outlook.com>
2024-04-14 13:44:10 +0100

    updated names of game resolution

--- main.cpp
+++ main.cpp
@@ -1,18 +1,27 @@
 #include "raylib.h"
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
 #define WINDOW_TITLE "Tile Tapping 2"
  
-int main(int argc, const char **argv) { 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+struct Vector2
+{
+    int x = 0;
+    int y = 0;
+};
+
+
+int main(int argc, const char **argv) 
+{ 
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(60);
  
     // Main loop
-    while(!WindowShouldClose()) {
+    while(!WindowShouldClose()) 
+    {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawText(WINDOW_TITLE, SCREEN_WIDTH/2, 100, 50, PURPLE);
+        DrawText(WINDOW_TITLE, WINDOW_WIDTH/2, 100, 50, PURPLE);
 
         EndDrawing();
     }
