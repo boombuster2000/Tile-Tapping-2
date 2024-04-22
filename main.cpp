76f21e77440a6839980e487ffd506e97ca396453
boombuster2000 <chanuka.williams@outlook.com>
2024-04-22 18:31:43 +0100

    implemented basic menu class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "vector"
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 #define WINDOW_TITLE "Tile Tapping 2"
@@ -15,15 +16,16 @@ struct Text
 };
 
 struct TextListNode 
-{
-	Text* text;
-	TextListNode* next;
-    TextListNode* before;
-	TextListNode() : text(nullptr), next(nullptr), before(nullptr) {}
-	TextListNode(Text* text) : text(text), next(nullptr), before(nullptr) {}
-	TextListNode(Text* text, TextListNode *next) : text(text), next(next), before(nullptr) {}
-	TextListNode(Text* text, TextListNode *next, TextListNode* before) : text(text), next(next), before(before) {}
-};
+    {
+        Text* text;
+        TextListNode* next;
+        TextListNode* before;
+        TextListNode() : text(nullptr), next(nullptr), before(nullptr) {}
+        TextListNode(Text* text) : text(text), next(nullptr), before(nullptr) {}
+        TextListNode(Text* text, TextListNode *next) : text(text), next(next), before(nullptr) {}
+        TextListNode(Text* text, TextListNode *next, TextListNode* before) : text(text), next(next), before(before) {}
+    };
+
 
 /**
  * Calculates the centre position on the screen from the given text and font size.
@@ -42,6 +44,58 @@ Vector2 GetTextCenterPositionOnScreen(const Text text)
     return {((float)GetScreenWidth()/2) - offset, (float)GetScreenHeight()/2};
 }
 
+class Menu
+{
+    private:
+    int m_currentOptionIndex = 0;
+    const int m_fontEnlargment = 0;
+    std::vector<Text> m_options;
+
+    public:
+    // Constructor
+    Menu(std::vector<Text> options, int fontEnlargment) 
+    {   
+        m_options = options;
+        m_options[m_currentOptionIndex].fontSize += fontEnlargment;
+    }
+
+    Text GetCurrentOption()
+    {
+        return m_options[m_currentOptionIndex];
+    }
+
+    void ChangeToNextOption()
+    {
+        m_options[m_currentOptionIndex].fontSize -= m_fontEnlargment;
+
+        if (m_options.size() != m_currentOptionIndex+1) m_currentOptionIndex++;
+        else m_currentOptionIndex = 0;
+        
+        m_options[m_currentOptionIndex].fontSize += m_fontEnlargment;
+
+    }
+
+    void ChangeToPreviousOption()
+    {
+        m_options[m_currentOptionIndex].fontSize -= m_fontEnlargment;
+
+        if (m_currentOptionIndex-1 < 0) m_currentOptionIndex = m_options.size() - 1;
+        else m_currentOptionIndex--;
+
+        m_options[m_currentOptionIndex].fontSize += m_fontEnlargment;
+    }
+
+    void Render()
+    {
+        for (auto option:m_options)
+        {
+            option.position.x = GetTextCenterPositionOnScreen(option).x;
+            DrawTextWithStuct(option);
+        }
+    }
+};
+
+
 int main(int argc, const char **argv) 
 { 
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
@@ -52,13 +106,15 @@ int main(int argc, const char **argv)
     menuTitle.position.x = GetTextCenterPositionOnScreen(menuTitle).x;
     menuTitle.position.y = 50;
 
-    Text playButton = {"Play", 70, PINK};
+    Text playButton = {"Play", 50, PINK};
     playButton.position.x = GetTextCenterPositionOnScreen(playButton).x;
     playButton.position.y = 300;
 
     Text exitButton = {"Exit", 50, PINK};
     exitButton.position.x = GetTextCenterPositionOnScreen(exitButton).x;
     exitButton.position.y = playButton.position.y + 100;
+
+    Menu mainMenu({playButton, exitButton}, 20);
 
     // Circular Linked List
     TextListNode playButtonNode = {&playButton};
@@ -75,22 +131,11 @@ int main(int argc, const char **argv)
         ClearBackground(RAYWHITE);
         
         // Menu Input Handling
-        if (IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_UP))
-        {  
-            menuHead->text->fontSize = 50;
-            menuHead->text->position.x = GetTextCenterPositionOnScreen(*(menuHead->text)).x;
 
-            if (IsKeyPressed(KEY_DOWN)) menuHead = menuHead->next;
-            else if (IsKeyPressed(KEY_UP)) menuHead = menuHead->before;
-            
-            menuHead->text->fontSize = 70;
-            menuHead->text->position.x = GetTextCenterPositionOnScreen(*(menuHead->text)).x;
-        }
+        if (IsKeyPressed(KEY_DOWN)) mainMenu.ChangeToNextOption();
+        else if (IsKeyPressed(KEY_UP)) mainMenu.ChangeToPreviousOption();
+        mainMenu.Render();
 
-        //Draw Text
-        DrawTextWithStuct(menuTitle);
-        DrawTextWithStuct(playButton);
-        DrawTextWithStuct(exitButton);
         EndDrawing();
     }
     // Cleanup
